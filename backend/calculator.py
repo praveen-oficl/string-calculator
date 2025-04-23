b4bc52eb82bd322d68b30e3c30f658512a3410e3
praveen-oficl <praveen.oficl@gmail.com>
2025-04-23 22:07:18 +0530

    Throw exception with message on negative numbers

--- backend/calculator.py
+++ backend/calculator.py
@@ -5,13 +5,18 @@ def add(numbers):
         return 0
 
     delimiter = ",|\n"
-
     if numbers.startswith("//"):
-        match = re.match(r"//(.+)\n(.*)", numbers)
+        match = re.match(r"//(.)\n(.*)", numbers)
         if match:
             delimiter = re.escape(match.group(1))
             numbers = match.group(2)
 
     parts = re.split(delimiter, numbers)
     nums = list(map(int, parts))
+    negatives = [n for n in nums if n < 0]
+
+    if negatives:
+        raise Exception(f"negative numbers not allowed: {', '.join(map(str, negatives))}")
+
     return sum(nums)
+
